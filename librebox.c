c9c09bd540cd9e089f259b91c388d4b7f86a37dc
Ben <benjaminbesli@gmail.com>
2015-10-21 18:04:37 +0200

    test

--- librebox.c
+++ librebox.c
@@ -1,6 +1,30 @@
 #include <X11/Xlib.h>
+#include <stdio.h>
+
+Display *display;
+XEvent *event;
+
+
+void mainLoop() {
+	int running = 1;
+	
+	while(running) {
+		//Process event
+		XNextEvent(display, &event);
+		
+		if(event.type == KeyPress) {
+			printf("key press\n");
+		}
+	}
+}
 
 int main(void) {
 
+	//create display
+	if(!(display = XOpenDisplay(0x0)))
+		return 1;
+	
+	mainLoop();
+	
 	return 0;
 }
