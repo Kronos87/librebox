dc82da903421a4d9834370170c2de5d6e2335bf2
Ben <benjaminbesli@gmail.com>
2015-10-21 19:07:31 +0200

    test

--- librebox.c
+++ librebox.c
@@ -1,14 +1,17 @@
 #include <X11/Xlib.h>
+#include <stdio.h>
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 Display *display;
+Screen *screen;
 
 XEvent event;
 XWindowAttributes attr;
 XButtonEvent btnEvnt;
 
 void mainLoop() {
+	
 	int running = 1;
 	
 	while(running) {
@@ -24,11 +27,20 @@ void mainLoop() {
 			int xdiff = event.xbutton.x_root - btnEvnt.x_root;
 			int ydiff = event.xbutton.y_root - btnEvnt.y_root;
 			
+			/*
 			XMoveResizeWindow(display, btnEvnt.subwindow, 
 				attr.x + (btnEvnt.button == 1 ? xdiff : 0),
 				attr.y + (btnEvnt.button == 1 ? ydiff : 0),
-				MAX(1, attr.width + (btnEvnt.button == 2 ? xdiff : 0)),
-				MAX(1, attr.height + (btnEvnt.button == 2 ? ydiff : 0)));
+				MAX(1, attr.width + (btnEvnt.button == 3 ? xdiff : 0)),
+				MAX(1, attr.height + (btnEvnt.button == 3 ? ydiff : 0)));
+			*/
+			
+			if(btnEvnt.button == 1) {
+				if(event.xbutton.y_root <= 1) {
+					XMoveResizeWindow(display, btnEvnt.subwindow, 0, 0, )
+				}
+			}
+			
 		} else if(event.type == ButtonRelease) {
 			btnEvnt.subwindow = None;
 		}
@@ -41,9 +53,13 @@ int main(void) {
 	if(!(display = XOpenDisplay(NULL)))
 		return 1;
 	
+	screen = DefaultScreenOfDisplay(display);
+	printf("Screen: width=%d, height=%d\n", screen->width, screen->height);
+	
+	
 	XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("F1")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
 	XGrabButton(display, 1, Mod1Mask, DefaultRootWindow(display), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-	XGrabButton(display, 2, Mod1Mask, DefaultRootWindow(display), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+	XGrabButton(display, 3, Mod1Mask, DefaultRootWindow(display), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 	
 	btnEvnt.subwindow = None;
 	
